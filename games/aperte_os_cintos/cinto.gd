09f02d0f01fe7377dc4565f43cbf315ed6e55646
Gabrielle Santana <gabrielle.cordeiro.s@gmail.com>
2026-03-19 11:07:45 -0300

    Cintos reposicionáveis

--- games/aperte_os_cintos/cinto.gd
+++ games/aperte_os_cintos/cinto.gd
@@ -1,4 +1,11 @@
 extends Line2D
 
+class_name Cinto
+
+@export var Pa_Position: Vector2 = Vector2.ZERO
+@export var Pe_Position: Vector2 = Vector2.ZERO
+
+
+
 func _ready() -> void:
 	pass
--- games/aperte_os_cintos/cinto_passador.gd
+++ games/aperte_os_cintos/cinto_passador.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
-@onready var line_2d: Line2D = $".."
+@export var cinto: Cinto
+@onready var sprite_presilha: Sprite2D = $"../Cinto_Presilha/SpritePresilha"
 @onready var sprite_2d: Sprite2D = $Sprite2D
 @onready var cinto_presilha: Area2D = $"../Cinto_Presilha"
 
@@ -10,14 +11,15 @@ var touch_offset := Vector2.ZERO
 var travado := false
 
 func _ready() -> void:
-	start_position = position
-	line_2d.set_point_position(0, position)
-	line_2d.set_point_position(1, position)
-	position = start_position
+	start_position = cinto.Pa_Position
+	cinto_presilha.global_position = cinto.Pe_Position
+	global_position = start_position
+	cinto.set_point_position(0, global_position)
+	cinto.set_point_position(1, global_position)
 
 func _on_area_2d_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
 	if event is InputEventScreenTouch and event.pressed and not travado:
-		touch_offset = position - event.position
+		touch_offset = global_position - event.position
 		dragging = true
 
 
@@ -25,16 +27,16 @@ func _input(event: InputEvent) -> void:
 	if event is InputEventScreenTouch and not event.pressed:
 		dragging = false
 		if not travado:
-			position = start_position
-			line_2d.set_point_position(1, start_position)
+			global_position = start_position
+			cinto.set_point_position(1, start_position)
 
 	if event is InputEventScreenDrag and dragging and not travado:
-		position = event.position + touch_offset
-		line_2d.set_point_position(1, position)
+		global_position = event.position + touch_offset
+		cinto.set_point_position(1, global_position)
 
 func _on_cinto_presilha_area_entered(area: Area2D) -> void:
 	if area == self:
 		travado = true
 		dragging = false
-		position = cinto_presilha.position
-		line_2d.set_point_position(1, position)
+		global_position = cinto_presilha.global_position - Vector2(15, 17)
+		cinto.set_point_position(1, global_position)
