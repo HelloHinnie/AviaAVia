8f33ba2c0a691330bf7a4af822efb99ff9402b10
Gabrielle Santana <gabrielle.cordeiro.s@gmail.com>
2026-04-23 11:52:55 -0300

    Afivele Código

---
+++ games/afivele/path_2d.gd
@@ -0,0 +1,26 @@
+extends Path2D
+
+@onready var path_follow = $PathFollow2D
+var dragging = false
+
+func _input(event):
+	# Detecta o toque inicial no objeto (ou na tela)
+	if event is InputEventScreenTouch:
+		if event.pressed:
+			dragging = true
+		else:
+			dragging = false
+
+	# Movimentação
+	if event is InputEventScreenDrag and dragging:
+		update_position(event.position)
+
+func update_position(touch_pos: Vector2):
+	# 1. Converte a posição global do toque para a posição local do Path
+	var local_pos = to_local(touch_pos)
+	
+	# 2. Encontra o ponto mais próximo na curva em relação ao toque
+	var offset = curve.get_closest_offset(local_pos)
+	
+	# 3. Aplica o offset ao PathFollow2D para mover o objeto
+	path_follow.progress = offset
